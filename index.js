ad1792c4494c4930291a043406029bb0ead7a617
Hyrum89 <halvarad@bruinmail.slcc.edu>
2017-04-25 15:05:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var d = new Date();
 document.getElementById("date").innerHTML = d.toString();
 
       
+ <script>
 function myFunction() 
   { 
- document.getElementById("demo").innerHTML = "My name is Hyrum I'am 27 years old I have been leaving in the United States for 16 years.!";  document.getElementById("demo").style.color = "blue";
+ document.getElementById("demo").innerHTML = "My name is Hyrum I'am 27 years old I have been leaving in the United States for 16 years.!";  document.getElementById("demo").style.color = "black";
 }
-
+</script>
